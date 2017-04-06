8e9719d8df9fd6ae0a655994f8325b476c9fd728
Théo Segonds <theo.segonds@inria.fr>
2017-04-06 19:05:33 +0200

    Upgrade creature to avoid break with incompatibility of pypot 3 and poppy-ergo-jr<2

--- auto-update.sh
+++ auto-update.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 echo -e "Starting update on $(date)."
+
+# Quick fix for creature upgrade
+if `python -c "import poppy_ergo_jr"`; then
+  conda upgrade poppy-ergo-jr
+fi
+
 conda upgrade pypot
+
 echo -e "Done."
