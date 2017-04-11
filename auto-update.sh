fd0472f0944420cc99dc4a3aa8d99b3dbf127375
Théo Segonds <theo.segonds@inria.fr>
2017-04-11 13:38:30 +0200

    Upgrade for any creature available in conda

--- auto-update.sh
+++ auto-update.sh
@@ -2,10 +2,9 @@
 
 echo -e "Starting update on $(date)."
 
-# Quick fix for creature upgrade
-if `python -c "import poppy_ergo_jr"`; then
-  conda upgrade poppy-ergo-jr
-fi
+# WARNING puppet-master (web interface) is needed to have the installed creature
+creature=`grep "creature" $HOME/.poppy_config.yaml | awk -F ": " '{print $2}'`
+conda upgrade $creature
 
 conda upgrade pypot
 
