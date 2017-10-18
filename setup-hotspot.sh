dfe99172bd66763a62c57c0867690a833054bef8
Damien Caselli <damien.caselli@gmail.com>
2017-10-18 14:55:38 +0200

    Upgrade hotspot service

--- setup-hotspot.sh
+++ setup-hotspot.sh
@@ -4,7 +4,7 @@ if [ "$(hrpi-version)" = "rpi-3" ]; then
     poppy_hostname="$1"
 
     cd /tmp || exit
-    wget -O rpi3-hotspot.zip "https://gitlab.inria.fr/dcaselli/rpi3-hotspot/repository/archive.zip?ref=2.0.1"
+    wget -O rpi3-hotspot.zip "https://gitlab.inria.fr/dcaselli/rpi3-hotspot/repository/archive.zip?ref=2.1.0"
     unzip rpi3-hotspot.zip
     rm rpi3-hotspot.zip
     mv rpi3-hotspot* rpi3-hotspot
