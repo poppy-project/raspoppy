08ed96400ef962ecb99aea0d50b519289e26f065
Jean-Luc Charles <jean-luc.charles@ensam.eu>
2019-02-10 18:29:36 +0100

    Update setup-hotspot.sh
    Variable name "hrpi-version" is incorrect and gives a shell error.
    Replace "hrpi-version"  by  "hrpi_version".
    
    By the way I wonder where this varaible is set in the installation process ?

--- setup-hotspot.sh
+++ setup-hotspot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(hrpi-version)" = "rpi-3" ]; then
+if [ "$(hrpi_version)" = "rpi-3" ]; then
     cd /tmp || exit
     wget -O rpi3-hotspot.zip "https://gitlab.inria.fr/dcaselli/rpi3-hotspot/repository/archive.zip?ref=3.0.1"
     unzip rpi3-hotspot.zip
