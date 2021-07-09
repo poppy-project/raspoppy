abf35cbab2f0fadb8834405b71d00b038f802e6c
Antoine Pringalle <pringalle.a@gmail.com>
2021-07-09 17:46:54 +0200

    feat(setup-poppy): Cloning scratch branch of raspoppy

--- raspoppyfication.sh
+++ raspoppyfication.sh
@@ -69,7 +69,9 @@ poppy_hostname=${poppy_hostname:-"poppy"}
 git_branch=${git_branch:-"master"}
 reboot_after_install=${reboot_after_install:-0}
 
-url_root="https://raw.githubusercontent.com/poppy-project/raspoppy/$git_branch"
+# scratch_installation
+raspoppy_branch="scratch-beta"
+url_root="https://raw.githubusercontent.com/poppy-project/raspoppy/$raspoppy_branch"
 
 cd /tmp || exit
 echo -e " "
